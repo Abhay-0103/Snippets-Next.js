2991346bb050d0f62341e039c5d0f951e23da2ed
Abhay-0103 <ab0321054@gmail.com>
2025-10-16 14:23:11 +0530

    cretaed save Snippet

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -4,13 +4,23 @@ import { Editor } from "@monaco-editor/react";
 import React, { useState } from "react";
 import type { Snippet } from "@prisma/client";
 import { Button } from "./ui/button";
+import { saveSnippet} from "@/actions"
 
 const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
   const [code, setCode] = useState(snippet.code);
 
+  const changeEventHandler = (value:string = "") => {
+    setCode(value);
+  }
+
+  // cannot use , use server actios as a inline client component or page
+  // it should be in another folder or file 
+
+  const saveSnippetActions = saveSnippet.bind(null, snippet.id, code);
+
   return (
     <div className="flex flex-col gap-4">
-        <form className="flex items-center justify-between p-4">
+        <form action={saveSnippetActions} className="flex items-center justify-between p-4">
             <h1 className="font-bold text-xl">Edit Your Code Here : </h1>
             <Button type="submit" className=" bg-amber-500 ">Save</Button>
         </form>
@@ -20,6 +30,7 @@ const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
         defaultLanguage="javascript"
         theme="vs-dark"
         defaultValue={code}
+        onChange={changeEventHandler}
       />
     </div>
   );
