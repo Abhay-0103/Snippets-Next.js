20ceafcb887f3bb0d32d5a4b6aa8dac38099314c
Abhay-0103 <ab0321054@gmail.com>
2025-10-16 14:01:32 +0530

    created  folder routing in snippet

---
+++ src/app/snippet/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import EditSnippetForm from "@/components/EditSnippetForm";
+import { prisma } from "@/lib/prisma";
+import { Snippet } from "next/font/google";
+import React from "react";
+
+const EditPageSnippet = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = parseInt((await params).id);
+  const snippet = await prisma.snippet.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!snippet) {
+    return <h1>Snippet Not Found</h1>;
+  }
+
+  return (
+    <div>
+      <EditSnippetForm snippet = {snippet} />
+    </div>
+  );
+};
+
+export default EditPageSnippet;
