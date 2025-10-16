a4e251e81ed1b00cfe8f55541d3318b60034e6e3
Abhay-0103 <ab0321054@gmail.com>
2025-10-16 14:00:07 +0530

    Added snippet and Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const snippets = await prisma.snippet.findMany();
+
   return (
     <div>
       <h1 className="font-bold text-4xl">Home</h1>
       <div className="flex items-center justify-between">
         <h1 className="mt-4 text-2xl">Snippets</h1>
-        <Link href="/snippet/new"><Button className="cursor-pointer bg-amber-800">New</Button></Link>
-      </div>
+        <Link href="/snippet/new">
+          <Button className="cursor-pointer bg-amber-800">New</Button>
+        </Link>
+      </div >
+      {snippets.map((snippet) => (
+        <div key={snippet.id} className="flex items-center justify-between p-2 mt-2 rounded-md my-2 bg-gray-700">
+          <h1 className="p-3">{snippet.title}</h1>
+          <Link href={`/snippet/${snippet.id}`} className="p-4">
+            <Button className="bg-amber-600 mt-2" variant={'link'}>View</Button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
