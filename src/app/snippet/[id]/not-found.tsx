2ec8d70777c537d8f22bff23b8519e858c9aa640
Abhay-0103 <ab0321054@gmail.com>
2025-10-17 11:58:12 +0530

    File base not-found page

---
+++ src/app/snippet/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SnippetNotFound = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <h1>Snippet Not Found</h1>
+        </div>
+  )
+}
+
+export default SnippetNotFound
