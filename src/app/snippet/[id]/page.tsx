3d348cb931c6c89fba35cb80a742dc7198fec7bc
Abhay-0103 <ab0321054@gmail.com>
2025-10-17 12:00:01 +0530

    imported notfound

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import React from "react";
+import * as actions from "@/actions";
+import { notFound } from "next/navigation";
 
 const SnippetDetailPage = async ({
   params,
@@ -9,6 +11,8 @@ const SnippetDetailPage = async ({
   params: Promise<{ id: string }>;
 }) => {
   const id = parseInt((await params).id);
+  
+  await new Promise((r) => setTimeout(r, 2000) );
 
   const snippet = await prisma.snippet.findUnique({
     where: {
@@ -16,19 +20,23 @@ const SnippetDetailPage = async ({
     },
   });
 
-  if (!snippet) {
-    return <h1>Snippet Not Found</h1>;
-  }
+  if (!snippet) notFound();
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
 
   return (
     <div className="flex flex-col gap-2.5">
       <div className="flex items-center justify-between">
         <h1 className="font-bold text-xl">{snippet.title}</h1>
         <div className="flex items-center gap-3">
-          <Link href={`/snippet/${snippet.id}/edit`}><Button className="bg-amber-500">Edit</Button></Link>
-          <Button variant={"destructive"} className="bg-red-600">
-            Delete
-          </Button>
+          <Link href={`/snippet/${snippet.id}/edit`}>
+            <Button className="bg-amber-500">Edit</Button>
+          </Link>
+          <form action={deleteSnippetAction}>
+            <Button variant={"destructive"} type="submit" className="bg-red-600">
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
       <pre className="p-4 mt-3 bg-indigo-600 rounded border-amber-600">
