a2bc28d1c6a7be41a5b54787789be48c0ba5961a
Abhay-0103 <ab0321054@gmail.com>
2025-10-17 11:59:02 +0530

    Added save & deleted snippet handler

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -4,27 +4,32 @@ import { Editor } from "@monaco-editor/react";
 import React, { useState } from "react";
 import type { Snippet } from "@prisma/client";
 import { Button } from "./ui/button";
-import { saveSnippet} from "@/actions"
+import { saveSnippet } from "@/actions";
 
 const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
   const [code, setCode] = useState(snippet.code);
 
-  const changeEventHandler = (value:string = "") => {
+  const changeEventHandler = (value: string = "") => {
     setCode(value);
-  }
+  };
 
   // cannot use , use server actios as a inline client component or page
-  // it should be in another folder or file 
+  // it should be in another folder or file
 
   const saveSnippetActions = saveSnippet.bind(null, snippet.id, code);
 
   return (
     <div className="flex flex-col gap-4">
-        <form action={saveSnippetActions} className="flex items-center justify-between p-4">
-            <h1 className="font-bold text-xl">Edit Your Code Here : </h1>
-            <Button type="submit" className=" bg-amber-500 ">Save</Button>
-        </form>
-        
+      <form
+        action={saveSnippetActions}
+        className="flex items-center justify-between p-4"
+      >
+        <h1 className="font-bold text-xl">Edit Your Code Here : </h1>
+        <Button type="submit" className=" bg-amber-500 ">
+          Save
+        </Button>
+      </form>
+
       <Editor
         height="40vh"
         defaultLanguage="javascript"
